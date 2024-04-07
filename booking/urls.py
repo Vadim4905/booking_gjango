3673a00102afa9db7389e6d4b1525e7b21606fd2
Vadym Hotko <poipooo600@gmail.com>
2024-04-07 10:52:39 +0100

    added botstrap styles

--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('rooms/', views.get_room_list,name='rooms'),
-    path('rooms/<int:pk>/', views.get_room_details,name='room'),
+    path('', views.get_room_list,name='rooms'),
+    path('room/<int:pk>/', views.get_room_details,name='room-detail'),
+    path('booking/<int:pk>/', views.get_booking_detail,name='booking-detail'),
+    path('booking/create', views.booking_from,name='booking-form'),
 ]
